827f378193310b846519b64694b79e36a7ed23ca
Rob Garbanati <robert.garbanati@automotivepower.com>
2020-11-19 16:33:00 -0800

    ran vtraceenable and still runs!

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -23,7 +23,6 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -86,7 +85,7 @@ int main(void)
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
-
+  vTraceEnable(TRC_START);
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
